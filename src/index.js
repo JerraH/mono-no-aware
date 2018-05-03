00160a5e5b34cf3893e1479c265bcef054edd888
Evie Salomon <evie@floozyinc.com>
2018-05-03 12:40:45 -0400

    added store

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import Phaser from 'phaser';
 import TitleScene from './TitleScene';
 import GameScene from './GameScene';
 import EndingScene from './EndingScene';
+import store from './store';
 
-var config = {
+let config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
     width: 800,
@@ -17,6 +18,7 @@ var config = {
 };
 
 let game = new Phaser.Game(config);
+
 game.scene.add('title', new TitleScene());
 game.scene.add('game', new GameScene());
 game.scene.add('ending', new EndingScene());
