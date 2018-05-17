083a1a25b4f0022d7963e0c3c21f5517e07e0cc4
Evie Salomon <evie@floozyinc.com>
2018-05-17 15:01:37 -0400

    ending music

--- src/scenes/EndingScene.js
+++ src/scenes/EndingScene.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser';
+import store from '../store';
 
 export default class EndingScene extends Phaser.Scene {
 
@@ -6,12 +7,21 @@ export default class EndingScene extends Phaser.Scene {
         this.load.image('archer', 'assets/images/archer.png')
         this.load.image('arched', 'assets/images/arched.png')
         this.load.image('palace', 'assets/images/palace.jpg')
-
+        this.load.audio('ending', 'assets/audio/ending.m4a')        
     }
 
     create() {
         this.background = this.add.image(400, 300, 'palace')
         this.archer = this.add.image(260, 300, 'archer')
+
+        let music = store.getMusic();
+        if (music) {
+            music.stop();
+            store.setMusic();
+        }
+
+        let theme = this.sound.add('ending');
+        theme.play({ volume: 0.5 });
 
         setTimeout(() => {
             this.archer.destroy()
